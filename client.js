c75ca8cb6fbe615b845d93feef2d01d5d4918e60
lzkelley <lkelley@cfa.harvard.edu>
2018-01-14 15:14:07 -0500

    Add a 'timeoutFunc' which is called when the client timesout.  This can be set to an external function, for example to restart the client and server.

--- client.js
+++ client.js
@@ -19,6 +19,11 @@ var SOCKET_TYPE = "pair";
  * On the `PyServer` side, the given function is called with the given argument,
  * and the return value is passed on the subsequent heartbeat response.
  *
+ * When this `JSClient` doesn't get a response within a given time (`timeout`), the method,
+ * `JSClient.timeoutFunc` will be called.  This can be reset to point to a function which does
+ * some sort of recovery action, for example: restarting/recreating the `PyServer` and creating
+ * a new JSClient.
+ *
  */
 class JSClient {
     /**
@@ -28,13 +33,13 @@ class JSClient {
      * @param {str} port - Port to access on URI.
      * @param {int} timeout - Timeout interval in milliseconds.
      */
-    constructor(uri="127.0.0.1", port="3000", timeout=5000, debug=false) {
+    constructor(uri="127.0.0.1", port="3000", timeout=5000, debug=false, timeoutFunc=null) {
         let addr = "tcp://" + uri + ":" + port;
-        console.log("Initializing %s socket", SOCKET_TYPE);
+        this._log("Initializing %s socket", SOCKET_TYPE, debug=true);
         let socket = zmq.socket(SOCKET_TYPE);
-        // sock.bindSync('tcp://127.0.0.1:3000');
+        console.log("Starting connection to", addr);
         socket.connect(addr);
-        console.log('Connected to ', addr);
+        this._log('Connected to ', addr);
 
         this.addr = addr;
         this._socket = socket;
@@ -44,6 +49,14 @@ class JSClient {
         this._payload_name = null;
         this._payload_arg = null;
         this._payload_func = null;
+        if (timeoutFunc == null) {
+            timeoutFunc = this._timeoutError;
+        }
+        this.timeoutFunc = timeoutFunc;
+    }
+
+    _timeoutError() {
+        throw new Error("Connection timeout!");
     }
 
     /**
@@ -53,7 +66,7 @@ class JSClient {
         let self = this;
         // Send payload after heartbeat
         if (this._payload_name != null) {
-            console.log("\tSending beat with payload");
+            this._log("\tSending beat with payload");
             this._socket.send("beat", zmq.ZMQ_SNDMORE);
             this._socket.send(this._payload_name, zmq.ZMQ_SNDMORE);
             this._socket.send(this._payload_arg);
@@ -63,14 +76,14 @@ class JSClient {
         }
         // No payload to send
         else {
-            console.log("\tSending beat");
+            this._log("\tSending beat");
             this._socket.send("beat");
         }
 
         this._killer = setTimeout(function() {
-          console.log("Nothing received. Exiting...");
-          self._socket.close();
-          throw new Error("Connection timeout!")
+            self._log("Nothing received. Exiting...");
+            self._socket.close();
+            self.timeoutFunc();
         }, this._timeout);
     }
 
@@ -80,7 +93,7 @@ class JSClient {
     run() {
       let self = this;
       this._socket.on('message', function(msg) {
-        console.log("recieved: " + msg);
+        self._log("recieved: " + msg);
         clearTimeout(self._killer);
 
         let ind = msg.indexOf(PAYLOAD_STR);
@@ -90,11 +103,13 @@ class JSClient {
             let error = (response.substr(0, 5) == "error");
 
             if (self._payload_func == null || error) {
-                console.log("Response: '%s'", response);
+                self._log("Response: '%s'", response);
                 if (self._payload_func == null) {
-                    throw new Error("Received payload response, no function set!");
+                    let err = "Received payload response, no function set!";
+                    console.error(err);
+                    // throw new Error(err);
                 } else {
-                    console.log("Error returned, not calling response function!");
+                    self._log("Error returned, not calling response function!");
                 }
             } else {
                 self._payload_func(response);
@@ -102,7 +117,7 @@ class JSClient {
             // Reset payload response function
             self._payload_func = null;
         } else if (self._payload_func != null ) {
-            console.log("WARNING: payload function set, but no return value!")
+            self._log("WARNING: payload function set, but no return value!")
         }
 
         self._beat();
@@ -130,54 +145,50 @@ class JSClient {
         var stopFailed;
         var self = this;
         this._socket.on('message', function(msg) {
-          console.log("recieved: " + msg);
+          self._log("recieved: " + msg);
           if (msg == "STOPPED") {
               clearTimeout(self._killer);
-              console.log("Server successfully exited.")
-              // process.exit(0);
-              this._socket.send("STOP");
+              self._log("Server successfully exited.")
+              self._socket.send("STOP");
           }
         });
 
         stopFailed = setTimeout(function() {
-            console.log("No stopped signal recieved!");
+            self._log("No stopped signal recieved!");
             self._socket.close();
             throw new Error("Connection timeout during stop signal!")
         }, this._timeout);
 
     }
 
-    _log(msg) {
-        if (this._debug) {
-            console.log(msg);
+    _log(msg, debug=null) {
+        // Set default debug value
+        if (debug == null) {
+            debug = this._debug;
+        }
+
+        if (debug) {
+            let _msg = getDateTime() + " :: " + msg;
+            console.log(_msg);
         }
     }
 
 }
 
 function getDateTime() {
-
     var date = new Date();
-
     var hour = date.getHours();
     hour = (hour < 10 ? "0" : "") + hour;
-
     var min  = date.getMinutes();
     min = (min < 10 ? "0" : "") + min;
-
     var sec  = date.getSeconds();
     sec = (sec < 10 ? "0" : "") + sec;
-
     var year = date.getFullYear();
-
     var month = date.getMonth() + 1;
     month = (month < 10 ? "0" : "") + month;
-
     var day  = date.getDate();
     day = (day < 10 ? "0" : "") + day;
-
-    return year + ":" + month + ":" + day + ":" + hour + ":" + min + ":" + sec;
-
+    return year + ":" + month + ":" + day + " " + hour + ":" + min + ":" + sec;
 }
 
 module.exports = JSClient;
